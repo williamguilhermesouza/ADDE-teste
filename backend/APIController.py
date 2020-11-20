10fccb5f7dc52b8f58a2fa1be3b0ab0522c1c06b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-20 12:45:32 -0300

    included caching of 15 minutes

--- backend/APIController.py
+++ backend/APIController.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_caching import Cache
 
 import APIService
 
-app = Flask(__name__)
+## configuring cache
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 900000
+}
 
+## initializing flask with default __name__
+app = Flask(__name__)
+app.config.from_mapping(config)
+
+## initializing cache
+cache = Cache(app)
+
+## /city/region main route, with cache decorators
 @app.route('/<city>/<region>')
+@cache.memoize(900000)
 def getWeather(city, region):
     return APIService.getWeather(city, region)
--- backend/APIService.py
+++ backend/APIService.py
@@ -1,17 +1,22 @@
 import requests
   
+## main get weather request
 def getWeather(city, region):
     url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
+    ## building the query to the api
     querystring = {"q":f'{city},{region}',"units":"metric"}
 
+    ## headers of the query with the key and host
     headers = {
         'x-rapidapi-key': "71d88ab42fmshe11d43503705b84p13f999jsn02e75f42af27",
         'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
         }
 
+    ## request response object to be treated
     api_response = requests.request("GET", url, headers=headers, params=querystring)
 
+    ## parsing response to json and extracting desired data
     json_response = api_response.json()
     temperature = json_response['main']['temp']
     city = json_response['name']
@@ -19,6 +24,7 @@ def getWeather(city, region):
     icon_code = json_response['weather'][0]['icon']
     icon_url = f'http://openweathermap.org/img/w/{icon_code}.png'
 
+    ## creating desired object with data
     response = { 
         'city': city, 
         'weather': weather,
@@ -26,6 +32,7 @@ def getWeather(city, region):
         'icon_code':icon_code,
         'icon_url': icon_url
          }
-         
+
+    ## returning desired response
     print(response)
     return response
