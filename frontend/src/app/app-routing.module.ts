06d8ca7751aa51aae93677b829bcd5389d292f3c
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-20 14:54:35 -0300

    working on raw frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'get-weather', component: WeatherComponent },
+  {path: '', redirectTo: '/get-weather', pathMatch: 'full'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
