de1629b6c3fc515e86e436abbc4833bfad8da7e8
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-21 21:10:55 -0300

    implemented tsparticles background

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tsParticles } from 'tsparticles';
+
+import particlesConfig from './default.js';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   preserveWhitespaces: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  ngOnInit(): any {
+    tsParticles.load('tsparticles', particlesConfig);
+  }
 }
